1981fdde6fab490690d63172ea2662b047afad32
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-03-24 22:44:15 -0500

    added robbin skills

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Robbin__Season
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan you Robbery Big Pat");
+
+            var robber1 = new Robber();
+
+            Console.WriteLine("Please enter Robber Name");
+            robber1.Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter robber's skill level between 0 to 10.");
+            robber1.SkillLevel = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter robber's courage factor 0.0 to 2.0");
+            robber1.CougarFactor = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{robber1.Name} has robbing skill: {robber1.SkillLevel} and courage factor of {robber1.CougarFactor}");
         }
     }
 }
--- Robber.cs
+++ Robber.cs
@@ -6,5 +6,38 @@ namespace Robbin__Season
 {
     class Robber
     {
+        private int _skillLevel;
+        private decimal _courageFactor;
+        public string Name { get; set; }
+
+        public int SkillLevel {
+            get
+            {
+                return _skillLevel;
+            }
+            set
+            {
+                if (value > 0 && value < 11)
+                {
+                    _skillLevel = value;
+                }
+            }
+        }
+
+        public decimal CougarFactor
+        {
+            get
+            {
+                return _courageFactor;
+            }
+            set
+            {
+                if(value >= 0.0m && value <= 2.0m)
+                {
+                    _courageFactor = value;
+                }
+            }
+        }
+        
     }
 }
